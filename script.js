d92ab4cef381408151b84cd2d44fa70f691a6aba
Natan Tibebu <natantibebu111@gmail.com>
2025-08-15 09:30:28 +0300

    Make the button chnage with color

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var index = 0;
 function ChangeColor(){
     var colors = ["red","blue","orange","yellow","green","purple"];
+    const name = document.getElementById('colorBtn');
+    name.innerText = colors[index];
     document.getElementsByTagName("body")[0].style.background=colors[index++];
     if(index > colors.length -1){
         index = 0;
     }
+    
 }
